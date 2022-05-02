18b9cdb4a7d9085a8b4057b666c51b261bdbeba9
sepehr alipour <alipour.dev@gmail.com>
2022-05-02 11:40:45 -0400

    Add files via upload

---
+++ WS01/w1p1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	printf("+-------------------------------+\n");
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\n");
+	printf("Sepehr Alipour\n");
+	return 0;
+}
