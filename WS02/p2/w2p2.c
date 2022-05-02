38e137564fbe1b5f6433ef27dbc6b904a293479b
sepehr alipour <alipour.dev@gmail.com>
2022-05-02 11:41:58 -0400

    Add files via upload
    submit ws2 part two

--- WS02/p2/w2p2.c
+++ WS02/p2/w2p2.c
@@ -1,30 +1,53 @@
-/*
-*****************************************************************************
-                          Workshop - #2 (P2)
-Full Name  : 
-Student ID#: 
-Email      : 
-Section    : 
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-*****************************************************************************
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
+int main() {
+    double userInput;
+    int tonnie = 200;
+    int loonie = 100;
+    int quarter = 25;
+    int dimes = 10;
+    int nickels = 5;
+    int pennies = 1;
+    float serviceFee;
+    int balance;
+    float remaining;
+    int coinNumber;
 
-int main(void)
-{
-    // You will need this when converting from grams to pounds (lbs)
-    const double GRAMS_IN_LBS = 453.5924;
 
-    // Note:
-    // You can convert Celsius to Fahrenheit given the following formula:
-    // fahrenheit = (celsius * 1.8) + 32.0);
+    printf("Change Maker Machine\n====================\n");
+    printf("Enter dollars and cents amount to convert to coins: $");
+    scanf("%lf", &userInput);
+    userInput *= 100; // convert to cent
+    serviceFee = (userInput * 5) / 100; //5% of payment
+    printf("Service fee (5.0 percent): %.2f\n", serviceFee / 100);//show as dollar
+    balance = userInput - serviceFee;
+    printf("Balance to dispense: $%.2lf", (float)balance / 100);
+    printf("\n");
+
+    remaining = balance % tonnie;
+    printf("\n$%.2lf Toonies  X %d (remaining: $%.2lf)\n", (float)tonnie / 100, balance / tonnie, remaining / 100);
+
+    coinNumber = remaining / loonie;
+    remaining = (int)remaining % loonie;
+    printf("$%.2lf Loonies  X %d (remaining: $%.2lf)\n", (float)loonie / 100, coinNumber, remaining / 100);
+
+    coinNumber = remaining / quarter;
+    remaining = (int)remaining % quarter;
+    printf("$%.2lf Quarters X %d (remaining: $%.2lf)\n", (float)quarter / 100, coinNumber, remaining / 100);
+
+    coinNumber = remaining / dimes;
+    remaining = (int)remaining % dimes;
+    printf("$%.2lf Dimes    X %d (remaining: $%.2lf)\n", (float)dimes / 100, coinNumber, remaining / 100);
+
+    coinNumber = remaining / nickels;
+    remaining = (int)remaining % nickels;
+    printf("$%.2lf Nickels  X %d (remaining: $%.2lf)\n", (float)nickels / 100, coinNumber, remaining / 100);
+
+    coinNumber = remaining / pennies;
+    remaining = (int)remaining % pennies;
+    printf("$%.2lf Pennies  X %d (remaining: $%.2lf)\n", (float)pennies / 100, coinNumber, remaining / 100);
+
+    printf("\nAll coins dispensed!\n");
 
     return 0;
 }
