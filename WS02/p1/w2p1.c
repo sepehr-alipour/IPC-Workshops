84b00591981edbebfffd1246f04cb41869a985f7
sepehr alipour <alipour.dev@gmail.com>
2022-05-02 11:41:29 -0400

    Add files via upload
    submit ws1 part one

--- WS02/p1/w2p1.c
+++ WS02/p1/w2p1.c
@@ -1,25 +1,28 @@
-/*
-*****************************************************************************
-                          Workshop - #2 (P1)
-Full Name  : 
-Student ID#: 
-Email      : 
-Section    : 
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-*****************************************************************************
-*/
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+int main() {
+    double amount;
+    float tonnie = 2.00;
+    float loonie = 1.00;
+    float quarter = 0.25;
+    int division;
 
-int main(void)
-{
-    // You must use this variable in #3 data analysis section!
-    const double testValue = 330.99;
 
+    printf("Change Maker Machine\n====================\n");
+    printf("Enter dollars and cents amount to convert to coins: $");
+    scanf("%lf", &amount);
+    printf("\n");
+    division = amount / tonnie;
+    amount = amount - (division * tonnie);
+    printf("$%.2lf Toonies  X %d (remaining: $%.2lf)\n", tonnie, division, amount);
+    division = amount / loonie;
+    amount = amount - (division * loonie);
+    printf("$%.2lf Loonies  X %d (remaining: $%.2lf)\n", loonie, division, amount);
+    division = amount / quarter;
+    amount = amount - (division * quarter);
+    printf("$%.2lf Quarters X %d (remaining: $%.2lf)\n", quarter, division, amount);
+
+    printf("\nMachine error! Thank you for letting me keep $%.2lf!\n", amount);
 
     return 0;
 }
